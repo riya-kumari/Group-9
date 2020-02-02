8bd6d5d0913f985efec75c555471c42182c11637
Riya <riya.kumari@yahoo.com>
2020-02-02 11:15:24 -0800

    Update StatsWindow.py

--- StatsWindow.py
+++ StatsWindow.py
@@ -15,26 +15,30 @@ stats.insert(END, "Count: " )
 stats.insert(END, str(p.getCount()) + "\n")
 
 # 0 = x; 1 = y
+# x = true; y = false
 stats.insert(END, "Sum of X: " )
-stats.insert(END, str(p.findSum("x"))+ "\n")
+stats.insert(END, str(p.findSum(0))+ "\n")
 
 stats.insert(END, "Sum of Y: " )
-stats.insert(END, str(p.findSum("y"))+ "\n")
+stats.insert(END, str(p.findSum(1))+ "\n")
 
 stats.insert(END, "Mean of X: " )
-stats.insert(END, str(p.findMean("x")) + "\n" )
+stats.insert(END, str(p.findMean(0)) + "\n\n" )
 
 stats.insert(END, "Mean of Y: " )
-stats.insert(END, str(p.findMean("y")) + "\n" )
+stats.insert(END, str(p.findMean(1)) + "\n" )
 
 stats.insert(END, "Std Dev of X: "  )
-stats.insert(END, str(p.findStdDev("x")) + "\n" )
+stats.insert(END, str(p.findStdDev_X()) + "\n" )
 
 stats.insert(END, "Std Dev of Y: " )
-stats.insert(END, str(p.findStdDev("y")) + "\n" )
+stats.insert(END, str(p.findStdDev_Y()) + "\n" )
 
 stats.insert(END, "Variance of X: " )
-stats.insert(END, str(p.findVariance("x")) + "\n")
+stats.insert(END, str(p.findVariance(0)) + "\n")
+
+stats.insert(END, "Variance of Y: " )
+stats.insert(END, str(p.findVariance(0)) + "\n\n")
 
 stats.insert(END, "Slope: " )
 stats.insert(END, str(float(p.getSlope())) + "\n" )
@@ -48,14 +52,33 @@ stats.insert(END, str(p.getCorrCoef()) + "\n" )
 # p.displayBoth()
 
 
-# stats.insert(END, "Line: " )
-# stats.insert(END, str(p.getFormula()) + "\n")
+stats.insert(END, "Line: " )
+stats.insert(END, p.getFormula() + "\n")
+
+
+# stats.insert(END, "What to never do with Regression : " + "\n")
+# stats.insert(END, "1. Inferring Causation : THE TWO VARIABLE CAN BE CORRELATED BUT NEVER CAUSE ONE ANOTHER!!! " + "\n")
+# stats.insert(END, "2. " + "\n")
+# stats.insert(END, "What to never do with Regression : " + "\n")
+# stats.insert(END, "What to never do with Regression : " + "\n")
+
+
+
 
 # stats.insert(END, "Prediction for x: " )
 # stats.insert(END, str(p.getPrediction("5")) + "\n" )
 
 stats.pack()
+
 first.grid(row = 0, column = 0)
+buttons = LabelFrame(root)
+b1 = Button(buttons, text = "View Scatterplot", command = lambda:p.displayScatter())
+b1.pack()
+
+b2 = Button(buttons, text = "View Residuals Plot", command = lambda:p.displayResid())
+b2.pack()
+buttons.grid(row = 1, column = 0)
+
 
 
 
