05bfee214064f1f05e23edd08a94cb623d02a412
Riya <riya.kumari@yahoo.com>
2020-02-02 08:30:03 -0800

    Update StatsWindow.py

--- StatsWindow.py
+++ StatsWindow.py
@@ -2,50 +2,66 @@ from tkinter import*
 import PIL
 from PIL import ImageTk
 from PIL import Image
-# import LinearRegStats, plot
+import plot
+# Still need to do Line and Prediction for x
 
-
-# class window:
-#     def __init__(self):
-#         self.root = Tk()
-#         self.root.title("Stats-Window")
-#         self.root.geometry("200x200")
-
-#         self.graph = LabelFrame(self.root)
-#         self.graph.grid(row = 0, column =0)
-        
-
-#         self.stats = LabelFrame(self.root)
-#         sel
-#         self.count = Text(self.stats, "Count : ")
-#         self.sum = Text(self.stats, "Text : ")
-#         self.stats.grid(row = 1, column = 0)
-        
-#         self.root.mainloop()
-
-# result = window()
-
-        
 root = Tk()
-root.title("Stats-Window")
+p = plot.Graphs()
+# p.displayBoth()
+# All the Stats are recorded here
+first = LabelFrame(root)
+stats = Text(first, height = 20, width = 60)
+stats.insert(END, "Count: " )
+stats.insert(END, str(p.getCount()) + "\n")
 
-stats = Text(root, height = 20, width = 60)
-stats.insert(END, "Count:\n"  )
-stats.insert(END, "Sum:\n"  )
-stats.insert(END, "Mean:\n"  )
-stats.insert(END, "Std Dev of X:\n"  )
-stats.insert(END, "Std Dev of Y:\n"  )
-stats.insert(END, "Variance of X:\n\n\n"  )
+# 0 = x; 1 = y
+stats.insert(END, "Sum of X: " )
+stats.insert(END, str(p.findSum("x"))+ "\n")
 
-stats.insert(END, "Slope:\n"  )
-stats.insert(END, "Y-Intercept\n"  )
-stats.insert(END, "Correlation Coefficient:\n"  )
-stats.insert(END, "Line:\n"  )
-stats.insert(END, "Prediction for x\n"  )
+stats.insert(END, "Sum of Y: " )
+stats.insert(END, str(p.findSum("y"))+ "\n")
+
+stats.insert(END, "Mean of X: " )
+stats.insert(END, str(p.findMean("x")) + "\n" )
+
+stats.insert(END, "Mean of Y: " )
+stats.insert(END, str(p.findMean("y")) + "\n" )
+
+stats.insert(END, "Std Dev of X: "  )
+stats.insert(END, str(p.findStdDev("x")) + "\n" )
+
+stats.insert(END, "Std Dev of Y: " )
+stats.insert(END, str(p.findStdDev("y")) + "\n" )
+
+stats.insert(END, "Variance of X: " )
+stats.insert(END, str(p.findVariance("x")) + "\n")
+
+stats.insert(END, "Slope: " )
+stats.insert(END, str(float(p.getSlope())) + "\n" )
 
 
+stats.insert(END, "Y-Intercept:"  )
+stats.insert(END, str(float(p.getIntercept())) + "\n" )
+
+stats.insert(END, "Correlation Coefficient: " )
+stats.insert(END, str(p.getCorrCoef()) + "\n" )
+# p.displayBoth()
+
+
+# stats.insert(END, "Line: " )
+# stats.insert(END, str(p.getFormula()) + "\n")
+
+# stats.insert(END, "Prediction for x: " )
+# stats.insert(END, str(p.getPrediction("5")) + "\n" )
 
 stats.pack()
+first.grid(row = 0, column = 0)
+
+
+
+
+
 
 root.mainloop()
+
 
